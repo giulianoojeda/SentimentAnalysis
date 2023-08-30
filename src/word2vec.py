7f3b1e44568b4255730d5f57018958c5096621d5
Giuliano Ojeda <67844704+giulianoojeda@users.noreply.github.com>
2023-08-30 19:37:20 -0400

    added optional project

--- src/word2vec.py
+++ src/word2vec.py
@@ -59,3 +59,38 @@ def vectorizer(
 
     # Convert the list of vectors to a 2D array and return it
     return np.array(corpus_vectors)
+
+
+def vectorizer_w2v_v2(corpus, model, num_features):
+    corpus_vectors = []  # Initialize empty list to store the vectors for all documents
+
+    # Iterate over all documents in corpus
+    for doc in corpus:
+        # Initialize empty array to store the vector representation of the document
+        word_vectors = np.zeros((len(doc), num_features))
+        # Initialize counter to keep track of the number of words in the document
+        word_count = 0
+        # Iterate over all words in the document
+        for idx, word in enumerate(doc):
+            # Check if the word is present in the vocabulary
+            if word in model.key_to_index:
+                # If yes, then add its vector to the word_vectors array
+                word_vectors[idx] = model[word]
+                # Increment the counter by 1
+                word_count += 1
+
+        # Compute the average by dividing the sum of all word vectors by the number of words
+        if word_count == 0:
+            avg_vector = np.zeros(
+                (num_features,)
+            )  # If no word is present in the vocabulary, then return a vector of zeros
+        else:
+            avg_vector = np.mean(
+                word_vectors, axis=0
+            )  # Else, return the average vector of the document
+
+        # Append the average vector to the list of vectors
+        corpus_vectors.append(avg_vector)
+
+    # Convert the list of vectors to a 2D array and return it
+    return np.array(corpus_vectors)    
